982d33c85fb771b45bb499d4284514c2dcaa3aeb
valia <valiapima@gmail.com>
2021-10-31 18:08:11 +0800

    123

--- Assets/Scripts/UI/StudentInBus.cs
+++ Assets/Scripts/UI/StudentInBus.cs
@@ -10,11 +10,11 @@ public class StudentInBus : MonoBehaviour
 
         for(int i = 0; i < GameManager.Instance.Matches.Count; i++)
         {
-            var left = Instantiate(GameManager.Instance.Matches[i].Student1.StudentGO);
+            var left = Instantiate(GameManager.Instance.Matches[i].Student2.StudentGO);
             left.transform.SetParent(transform.GetChild(childIndex++));
             left.transform.localPosition = Vector3.zero;
 
-            var right = Instantiate(GameManager.Instance.Matches[i].Student2.StudentGO);
+            var right = Instantiate(GameManager.Instance.Matches[i].Student1.StudentGO);
             right.transform.SetParent(transform.GetChild(childIndex++));
             right.transform.localPosition = Vector3.zero;
         }
