78122a69fa3d26d7047575230b5b5d94379a6522
Hdm <2439846178@qq.com>
2021-10-31 16:36:36 +0800

    自动填充BUS界面学生

--- Assets/Scripts/UI/StudentInBus.cs
+++ Assets/Scripts/UI/StudentInBus.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class StudentInBus : MonoBehaviour
 {
-    
+    private void Start()
+    {
+        int childIndex = 0;
+
+        for(int i = 0; i < GameManager.Instance.Matches.Count; i++)
+        {
+            var left = Instantiate(GameManager.Instance.Matches[i].Student1.StudentGO);
+            left.transform.localPosition = Vector3.zero;
+            left.transform.SetParent(transform.GetChild(childIndex++));
+
+            var right = Instantiate(GameManager.Instance.Matches[i].Student2.StudentGO);
+            right.transform.localPosition = Vector3.zero;
+            right.transform.SetParent(transform.GetChild(childIndex++));
+        }
+    }
 }
